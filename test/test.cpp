499666f2cc48cbf97ca87ab2236ee3cc46f2db03
Bernard Bussy <brbitzbussy@gmail.com>
2019-08-13 21:15:28 +0200

    merge master

--- test/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <limits.h>
-#include "gtest/gtest.h"
-
-namespace
-{
-TEST(FirstTest, Negative)
-{
-}
-} // namespace
--- test/test.sh
+++
@@ -1,3 +0,0 @@
-g++ -std=c++11 -stdlib=libc++  test.cpp main.cpp -lgtest -lpthread -o test
-./test
-rm test
