5adad5436fde450ff546d6b2b412fb35d50f44dc
Bernard Bussy <brbitzbussy@gmail.com>
2019-08-13 21:07:15 +0200

    finally got some tests working

---
+++ test/test_gps/test_gps.cpp
@@ -0,0 +1,24 @@
+// #include <NMEASentence.h>
+#include <unity.h>
+
+// void setUp(void) {
+// // set stuff up here
+// }
+
+// void tearDown(void) {
+// // clean stuff up here
+// }
+
+void test_function_make_gps(void)
+{
+    TEST_ASSERT_EQUAL(32, 32);
+}
+
+int main(int argc, char **argv)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_function_make_gps);
+    UNITY_END();
+
+    return 0;
+}
