1c1e960b6b85051f97e77a2cc83e2f41013c9c7a
Bernard Bussy <brbitzbussy@gmail.com>
2019-08-16 18:41:59 +0200

    small progress in test

---
+++ test/test_gps/TestCase.h
@@ -0,0 +1,17 @@
+#ifndef TestCase_h
+#define TestCase_h
+
+class TestCase
+{
+private:
+    const char *nmeaSentenceString;
+
+public:
+    //
+    // Constructors and Destructors
+    //
+    TestCase();
+    ~TestCase();
+};
+
+#endif
--- test/test_gps/test_gps.cpp
+++ test/test_gps/test_gps.cpp
@@ -1,5 +1,8 @@
 #include <NMEASentence.h>
 #include <unity.h>
+#include <stdio.h>
+#include <string.h>
+#include "TestCase.h"
 
 // void setUp(void) {
 // // set stuff up here
@@ -9,11 +12,22 @@
 // // clean stuff up here
 // }
 
+const char *testString = "$GNRMC,112738.000,A,2608.9935,S,02808.1064,E,0.00,0.00,090619,,,A*6F";
+
 void test_function_make_gps(void)
 {
     gpsReading test;
     gpsReading expected;
     int test2;
+    NMEASentence nmeaSentence;
+
+    initialiseNMEASentence(&nmeaSentence);
+    for (int i = 0; i < strlen(testString); i++)
+    {
+        readCharForNMEASentence(&nmeaSentence, testString[i]);
+    }
+
+    TEST_ASSERT_EQUAL_INT(NMEASentenceErr_NoError, nmeaSentence.errorCode);
 }
 
 int main(int argc, char **argv)
