6ea7bb12f35fd2881d3ca582db8490a8a9eb9116
Bernard Bussy <brbitzbussy@gmail.com>
2019-08-16 22:27:13 +0200

    script update

--- test/test_gps/main.py
+++ test/test_gps/main.py
@@ -4,7 +4,7 @@ from numpy import float32
 from string import Template
 
 template = Template('''{
-        .inputTestString = "$$GNRMC,112738.000,A,2608.9935,S,02808.1064,E,0.00,0.00,090619,,,A*6F\\r\\n",
+        .inputTestString = "$$GNRMC,112738.000,A,$latitudeNMEA,$latDirection,02808.1064,E,0.00,0.00,090619,,,A*6F\\r\\n",
         .expectedReadingStartIndication = true,
         .expectedReadingCompleteIndication = true,
         .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
@@ -25,16 +25,16 @@ template = Template('''{
 if __name__ == '__main__':
     for i in range(1):
         # random latitude
-        latDirection = random.choice([('N', 1), ('S', -1)])
+        latDirection = random.choice([('N', 1.00), ('S', -1.00)])
         latitudeValue = float32(random.uniform(10, 89))
-
         latitudeFractionalParts = str(
-            round(math.modf(latitudeValue)[0]*60, 4)).split('.')
+            round(math.modf(latitudeValue)[0]*60.00, 4)).split('.')
         latitudeDegreePart = str(int(math.modf(latitudeValue)[1]))
-        print('0'*(2-len(latitudeDegreePart))+latitudeDegreePart+'0' *
-              (2-len(latitudeFractionalParts[0]))+latitudeFractionalParts[0]+'.'+'0'*(4-len(latitudeFractionalParts[1]))+latitudeFractionalParts[1])
+        latitudeNMEA = '0'*(2-len(latitudeDegreePart))+latitudeDegreePart+'0'*(2-len(
+            latitudeFractionalParts[0]))+latitudeFractionalParts[0]+'.'+'0'*(4-len(latitudeFractionalParts[1]))+latitudeFractionalParts[1]
+        signedLatitude = float32(round(latDirection[1]*(float32(float(latitudeNMEA[:latitudeNMEA.find(
+            '.')-2]))+float32(float(latitudeNMEA[latitudeNMEA.find('.')-2:]))/60.00), 4))
 
-        signedLatitude = float32(latDirection[1]*random.uniform(10, 89))
         latHexBytes = ['00' if b == 0 else hex(b).strip(
             '0x') for b in signedLatitude.tobytes('C')]
         latHexValue = ''.join(
@@ -47,9 +47,10 @@ if __name__ == '__main__':
             '0x') for b in signedLongitude.tobytes('C')]
         lonHexValue = ''.join(
             b if len(b) == 2 else '0' + b for b in lonHexBytes)
-        # print(template.substitute({
-        #     'signedLatitude': signedLatitude,
-        #     'latDirection': latDirection[0],
-        #     'signedLongitude': signedLongitude,
-        #     'lonDirection': lonDirection[0],
-        # }))
+        print(template.substitute({
+            'latitudeNMEA': latitudeNMEA,
+            'latDirection': latDirection[0],
+            'signedLatitude': signedLatitude,
+            'signedLongitude': signedLongitude,
+            'lonDirection': lonDirection[0],
+        }))
