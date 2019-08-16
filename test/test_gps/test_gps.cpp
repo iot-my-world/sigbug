ac5292d20eda2f80b89307bba4a581b4bbd5cc70
Bernard Bussy <brbitzbussy@gmail.com>
2019-08-16 18:52:50 +0200

    creating test

--- test/test_gps/test_gps.cpp
+++ test/test_gps/test_gps.cpp
@@ -17,7 +17,13 @@ const char *testString = "$GNRMC,112738.000,A,2608.9935,S,02808.1064,E,0.00,0.00
 void test_function_make_gps(void)
 {
     gpsReading test;
-    gpsReading expected;
+    struct gpsReading expected = {
+        .lat = 26.12,
+        .latDirection = 'S',
+        .lon = 32.12,
+        .lonDirection = 'E',
+        .error = NMEASentenceErr_processGPSNMEASentence_NoError,
+    };
     NMEASentence nmeaSentence;
 
     initialiseNMEASentence(&nmeaSentence);
@@ -25,10 +31,13 @@ void test_function_make_gps(void)
     {
         readCharForNMEASentence(&nmeaSentence, testString[i]);
     }
+    TEST_ASSERT_EQUAL_INT(NMEASentenceErr_NoError, nmeaSentence.errorCode);
     parseNMEASentence(&nmeaSentence);
     TEST_ASSERT_EQUAL_INT(NMEASentenceErr_NoError, nmeaSentence.errorCode);
     TEST_ASSERT_EQUAL_STRING("GN", nmeaSentence.talkerIdentifier);
     TEST_ASSERT_EQUAL_STRING("RMC", nmeaSentence.sentenceIdentifier);
+    process_GNRMC_NMEASentence(&nmeaSentence, &test);
+    TEST_ASSERT_EQUAL_INT(expected.error, test.error);
 }
 
 int main(int argc, char **argv)
