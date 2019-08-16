e5c735ab64422393289d819f30121a9fd38fa643
Bernard Bussy <brbitzbussy@gmail.com>
2019-08-16 21:04:55 +0200

    gps test with test array

--- test/test_gps/TestCase.h
+++ test/test_gps/TestCase.h
@@ -1,10 +1,20 @@
+#include <NMEASentence.h>
+
 #ifndef TestCase_h
 #define TestCase_h
 
 typedef struct testCase
 {
     const char *inputTestString;
+    bool expectedReadingStartIndication;
+    bool expectedReadingCompleteIndication;
+    char expectedPostReadingNMEASentenceErr;
 
+    char expectedPostParse_NMEASentenceErr;
+    const char *expectedNMEATalkerIdentifier;
+    const char *expectedNMEASentenceIdentifier;
+
+    gpsReading expectedGPSReading;
 } testCase;
 
 #endif
--- test/test_gps/test_gps.cpp
+++ test/test_gps/test_gps.cpp
@@ -2,6 +2,7 @@
 #include <unity.h>
 #include <stdio.h>
 #include <string.h>
+#include <array>
 #include "TestCase.h"
 
 // void setUp(void) {
@@ -12,40 +13,55 @@
 // // clean stuff up here
 // }
 
-const char *testString = "$GNRMC,112738.000,A,2608.9935,S,02808.1064,E,0.00,0.00,090619,,,A*6F\r\n";
+testCase cases[] = {
+    {
+        .inputTestString = "$GNRMC,112738.000,A,2608.9935,S,02808.1064,E,0.00,0.00,090619,,,A*6F\r\n",
+        .expectedReadingStartIndication = true,
+        .expectedReadingCompleteIndication = true,
+        .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
+
+        .expectedPostParse_NMEASentenceErr = NMEASentenceErr_NoError,
+        .expectedNMEATalkerIdentifier = "GN",
+        .expectedNMEASentenceIdentifier = "RMC",
+
+        .expectedGPSReading = (gpsReading){
+            .error = NMEASentenceErr_processGPSNMEASentence_NoError,
+            .lat = -26.1499,
+            .latDirection = 'S',
+            .lon = 28.1351,
+            .lonDirection = 'E',
+        },
+    },
+};
 
 void test_function_make_gps(void)
 {
-    gpsReading test;
-    struct gpsReading expected = {
-        .error = NMEASentenceErr_processGPSNMEASentence_NoError,
-        .lat = -26.1499,
-        .latDirection = 'S',
-        .lon = 28.1351,
-        .lonDirection = 'E',
-    };
-    NMEASentence nmeaSentence;
 
-    initialiseNMEASentence(&nmeaSentence);
-    for (int i = 0; i < strlen(testString); i++)
+    for (int caseIdx = 0; caseIdx < sizeof(cases) / sizeof(*cases); caseIdx++)
     {
-        readCharForNMEASentence(&nmeaSentence, testString[i]);
+        NMEASentence nmeaSentence;
+        gpsReading test;
+        initialiseNMEASentence(&nmeaSentence);
+        for (int i = 0; i < strlen(cases[caseIdx].inputTestString); i++)
+        {
+            readCharForNMEASentence(&nmeaSentence, cases[caseIdx].inputTestString[i]);
+        }
+        TEST_ASSERT_EQUAL_INT_MESSAGE(cases[caseIdx].expectedReadingStartIndication, nmeaSentence.readingStarted, "Reading Started Indication");
+        TEST_ASSERT_EQUAL_INT_MESSAGE(cases[caseIdx].expectedReadingCompleteIndication, nmeaSentence.readingComplete, "Reading Ended Indication");
+        TEST_ASSERT_EQUAL_INT(NMEASentenceErr_NoError, nmeaSentence.errorCode);
+
+        parseNMEASentence(&nmeaSentence);
+        TEST_ASSERT_EQUAL_INT(cases[caseIdx].expectedPostParse_NMEASentenceErr, nmeaSentence.errorCode);
+        TEST_ASSERT_EQUAL_STRING(cases[caseIdx].expectedNMEATalkerIdentifier, nmeaSentence.talkerIdentifier);
+        TEST_ASSERT_EQUAL_STRING(cases[caseIdx].expectedNMEASentenceIdentifier, nmeaSentence.sentenceIdentifier);
+
+        process_GNRMC_NMEASentence(&nmeaSentence, &test);
+        TEST_ASSERT_EQUAL_INT(cases[caseIdx].expectedGPSReading.error, test.error);
+        TEST_ASSERT_EQUAL_FLOAT(cases[caseIdx].expectedGPSReading.lat.f, test.lat.f);
+        TEST_ASSERT_EQUAL_INT(cases[caseIdx].expectedGPSReading.latDirection, test.latDirection);
+        TEST_ASSERT_EQUAL_FLOAT(cases[caseIdx].expectedGPSReading.lon.f, test.lon.f);
+        TEST_ASSERT_EQUAL_INT(cases[caseIdx].expectedGPSReading.lonDirection, test.lonDirection);
     }
-    TEST_ASSERT_EQUAL_INT_MESSAGE(1, nmeaSentence.readingStarted, "Reading Started Indication");
-    TEST_ASSERT_EQUAL_INT_MESSAGE(1, nmeaSentence.readingComplete, "Reading Ended Indication");
-    TEST_ASSERT_EQUAL_INT(NMEASentenceErr_NoError, nmeaSentence.errorCode);
-
-    parseNMEASentence(&nmeaSentence);
-    TEST_ASSERT_EQUAL_INT(NMEASentenceErr_NoError, nmeaSentence.errorCode);
-    TEST_ASSERT_EQUAL_STRING("GN", nmeaSentence.talkerIdentifier);
-    TEST_ASSERT_EQUAL_STRING("RMC", nmeaSentence.sentenceIdentifier);
-
-    process_GNRMC_NMEASentence(&nmeaSentence, &test);
-    TEST_ASSERT_EQUAL_INT(expected.error, test.error);
-    TEST_ASSERT_EQUAL_FLOAT(expected.lat.f, test.lat.f);
-    TEST_ASSERT_EQUAL_INT(expected.latDirection, test.latDirection);
-    TEST_ASSERT_EQUAL_FLOAT(expected.lon.f, test.lon.f);
-    TEST_ASSERT_EQUAL_INT(expected.lonDirection, test.lonDirection);
 }
 
 int main(int argc, char **argv)
