803dbbcd3976a32bb8c9b5416352aadac4a88cee
Bernard Bussy <brbitzbussy@gmail.com>
2019-08-16 22:08:08 +0200

    python gps data script

--- test/test_gps/main.py
+++ test/test_gps/main.py
@@ -1,4 +1,5 @@
 import random
+import math
 from numpy import float32
 from string import Template
 
@@ -14,33 +15,41 @@ template = Template('''{
 
         .expectedGPSReading = (gpsReading){
             .error = NMEASentenceErr_processGPSNMEASentence_NoError,
-            .lat = $latitude,
+            .lat = $signedLatitude,
             .latDirection = '$latDirection',
-            .lon = $longitude,
+            .lon = $signedLongitude,
             .lonDirection = '$lonDirection',
         },
 },''')
 
 if __name__ == '__main__':
-    for i in range(10):
+    for i in range(1):
         # random latitude
         latDirection = random.choice([('N', 1), ('S', -1)])
-        latitude = float32(latDirection[1]*random.uniform(10, 89))
+        latitudeValue = float32(random.uniform(10, 89))
+
+        latitudeFractionalParts = str(
+            round(math.modf(latitudeValue)[0]*60, 4)).split('.')
+        latitudeDegreePart = str(int(math.modf(latitudeValue)[1]))
+        print('0'*(2-len(latitudeDegreePart))+latitudeDegreePart+'0' *
+              (2-len(latitudeFractionalParts[0]))+latitudeFractionalParts[0]+'.'+'0'*(4-len(latitudeFractionalParts[1]))+latitudeFractionalParts[1])
+
+        signedLatitude = float32(latDirection[1]*random.uniform(10, 89))
         latHexBytes = ['00' if b == 0 else hex(b).strip(
-            '0x') for b in latitude.tobytes('C')]
+            '0x') for b in signedLatitude.tobytes('C')]
         latHexValue = ''.join(
             b if len(b) == 2 else '0' + b for b in latHexBytes)
 
         # random longitude
         lonDirection = random.choice([('E', 1), ('W', -1)])
-        longitude = float32(lonDirection[1]*random.uniform(10, 179))
+        signedLongitude = float32(lonDirection[1]*random.uniform(10, 179))
         lonHexBytes = ['00' if b == 0 else hex(b).strip(
-            '0x') for b in longitude.tobytes('C')]
+            '0x') for b in signedLongitude.tobytes('C')]
         lonHexValue = ''.join(
             b if len(b) == 2 else '0' + b for b in lonHexBytes)
-        print(template.substitute({
-            'latitude': latitude,
-            'latDirection': latDirection[0],
-            'longitude': longitude,
-            'lonDirection': lonDirection[0],
-        }))
+        # print(template.substitute({
+        #     'signedLatitude': signedLatitude,
+        #     'latDirection': latDirection[0],
+        #     'signedLongitude': signedLongitude,
+        #     'lonDirection': lonDirection[0],
+        # }))
