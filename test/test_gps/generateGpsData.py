e1e8cfd93bc00b4c1ae277bdd6203a85c90c9b39
Bernard Bussy <brbitzbussy@gmail.com>
2019-08-17 09:08:22 +0200

    update pep8

--- test/test_gps/generateGpsData.py
+++ test/test_gps/generateGpsData.py
@@ -3,8 +3,11 @@ import math
 from numpy import float32
 from string import Template
 
-template = Template('''{
-        .inputTestString = "$$GNRMC,112738.000,A,$latitudeNMEA,$latDirection,$longitudeNMEA,$lonDirection,0.00,0.00,090619,,,A*6F\\r\\n",
+nmeaSentenceTemplate = Template(
+    "$$GNRMC,112738.000,A,$latitudeNMEA,$latDirection,$longitudeNMEA,$lonDirection,0.00,0.00,090619,,,A")
+
+testCaseTemplate = Template('''{
+        .inputTestString = "$nmeaSentence\\r\\n",
         .expectedReadingStartIndication = true,
         .expectedReadingCompleteIndication = true,
         .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
@@ -22,46 +25,75 @@ template = Template('''{
         },
 },''')
 
+
+def calculate_checksum(value):
+    checksum = 0
+    for c in value:
+        checksum ^= ord(c)
+    checksum = hex(checksum).strip('0x')
+    return checksum if len(checksum) == 2 else '0'*(2 - len(checksum)) + checksum
+
+
+def generate_test_case(latitude, latitude_direction, longitude, longitude_direction):
+    # process latitude
+    latitude_fractional_parts = str(
+        round(math.modf(latitude)[0] * 60.00, 4)).split('.')
+    latitude_degree_part = str(int(math.modf(latitude)[1]))
+    latitude_nmea = '0' * (2 - len(latitude_degree_part)) + latitude_degree_part + '0' * (2 - len(
+        latitude_fractional_parts[0])) + latitude_fractional_parts[0] + '.' + latitude_fractional_parts[1] + '0' * (4 - len(latitude_fractional_parts[1]))
+    signed_latitude = float32(round(latitude_direction[1] * (float32(float(latitude_nmea[:latitude_nmea.find(
+        '.') - 2])) + float32(float(latitude_nmea[latitude_nmea.find('.') - 2:])) / 60.00), 4))
+    lat_hex_bytes = ['00' if b == 0 else hex(b).strip(
+        '0x') for b in signed_latitude.tobytes('C')]
+    lat_hex_value = ''.join(
+        b if len(b) == 2 else '0' + b for b in lat_hex_bytes)
+
+    # random longitude
+    longitude_fractional_parts = str(
+        round(math.modf(longitude)[0] * 60.00, 4)).split('.')
+    longitude_degree_part = str(int(math.modf(longitude)[1]))
+    longitude_nmea = '0' * (3 - len(longitude_degree_part)) + longitude_degree_part + '0' * (2 - len(
+        longitude_fractional_parts[0])) + longitude_fractional_parts[0] + '.' + longitude_fractional_parts[1] + '0' * (4 - len(longitude_fractional_parts[1]))
+    signed_longitude = float32(round(longitude_direction[1] * (float32(float(longitude_nmea[:longitude_nmea.find(
+        '.') - 2])) + float32(float(longitude_nmea[longitude_nmea.find('.') - 2:])) / 60.00), 4))
+    lon_hex_bytes = ['00' if b == 0 else hex(b).strip(
+        '0x') for b in signed_longitude.tobytes('C')]
+    lon_hex_value = ''.join(
+        b if len(b) == 2 else '0' + b for b in lon_hex_bytes)
+
+    # create sentence
+    nmea_sentence = nmeaSentenceTemplate.substitute({
+        'latitudeNMEA': latitude_nmea,
+        'latDirection': latitude_direction[0],
+
+        'longitudeNMEA': longitude_nmea,
+        'lonDirection': longitude_direction[0],
+    })
+
+    return testCaseTemplate.substitute({
+        'nmeaSentence': nmea_sentence + "*" + calculate_checksum(nmea_sentence[1:]),
+        'latDirection': latitude_direction[0],
+        'signedLatitude': signed_latitude,
+
+        'lonDirection': longitude_direction[0],
+        'signedLongitude': signed_longitude,
+    })
+
+
 if __name__ == '__main__':
-    for i in range(1):
+    # test = "$GNRMC,112738.000,A,2608.9935,S,02808.1064,E,0.00,0.00,090619,,,A*6F"
+    # checkSection = test[1:test.find('*')]
+    # print(calculate_checksum(checkSection))
+    # print(generate_test_case(26.1499, ('S', -1.00), 28.1351, ('E', 1.00)))
+
+    for i in range(10):
         # random latitude
         latDirection = random.choice([('N', 1.00), ('S', -1.00)])
         latitudeValue = float32(random.uniform(10, 89))
-        latitudeFractionalParts = str(
-            round(math.modf(latitudeValue)[0]*60.00, 4)).split('.')
-        latitudeDegreePart = str(int(math.modf(latitudeValue)[1]))
-        latitudeNMEA = '0'*(2-len(latitudeDegreePart))+latitudeDegreePart+'0'*(2-len(
-            latitudeFractionalParts[0]))+latitudeFractionalParts[0]+'.'+latitudeFractionalParts[1]+'0'*(4-len(latitudeFractionalParts[1]))
-        signedLatitude = float32(round(latDirection[1]*(float32(float(latitudeNMEA[:latitudeNMEA.find(
-            '.')-2]))+float32(float(latitudeNMEA[latitudeNMEA.find('.')-2:]))/60.00), 4))
-
-        latHexBytes = ['00' if b == 0 else hex(b).strip(
-            '0x') for b in signedLatitude.tobytes('C')]
-        latHexValue = ''.join(
-            b if len(b) == 2 else '0' + b for b in latHexBytes)
 
         # random longitude
         lonDirection = random.choice([('E', 1.00), ('W', -1.00)])
         longitudeValue = float32(random.uniform(10, 170))
-        longitudeFractionalParts = str(
-            round(math.modf(longitudeValue)[0]*60.00, 4)).split('.')
-        longitudeDegreePart = str(int(math.modf(longitudeValue)[1]))
-        longitudeNMEA = '0'*(3-len(longitudeDegreePart))+longitudeDegreePart+'0'*(2-len(
-            longitudeFractionalParts[0]))+longitudeFractionalParts[0]+'.'+longitudeFractionalParts[1]+'0'*(4-len(longitudeFractionalParts[1]))
-        signedLongitude = float32(round(lonDirection[1]*(float32(float(longitudeNMEA[:longitudeNMEA.find(
-            '.')-2]))+float32(float(longitudeNMEA[longitudeNMEA.find('.')-2:]))/60.00), 4))
 
-        lonHexBytes = ['00' if b == 0 else hex(b).strip(
-            '0x') for b in signedLongitude.tobytes('C')]
-        lonHexValue = ''.join(
-            b if len(b) == 2 else '0' + b for b in lonHexBytes)
-
-        print(template.substitute({
-            'latitudeNMEA': latitudeNMEA,
-            'latDirection': latDirection[0],
-            'signedLatitude': signedLatitude,
-
-            'longitudeNMEA': longitudeNMEA,
-            'lonDirection': lonDirection[0],
-            'signedLongitude': signedLongitude,
-        }))
+        print(generate_test_case(latitudeValue,
+                                 latDirection, longitudeValue, lonDirection))
--- test/test_gps/test_gps.cpp
+++ test/test_gps/test_gps.cpp
@@ -12,9 +12,24 @@
 // // clean stuff up here
 // }
 
+/*
+const char *testString = "$GNRMC,112738.000,A,2608.9935,S,02808.1064,E,0.00,0.00,090619,,,A*6F";
+
+void test_function_make_gps(void)
+{
+    gpsReading test;
+    struct gpsReading expected = {
+        .error = NMEASentenceErr_processGPSNMEASentence_NoError,
+        .lat = -26.1499,
+        .latDirection = 'S',
+        .lon = 28.1351,
+        .lonDirection = 'E',
+    };
+*/
+
 testCase cases[] = {
     {
-        .inputTestString = "$GNRMC,112738.000,A,1230.1054,N,07633.0038,W,0.00,0.00,090619,,,A*6F\r\n",
+        .inputTestString = "$GNRMC,112738.000,A,1024.8696,S,11338.0173,E,0.00,0.00,090619,,,A*6f\r\n",
         .expectedReadingStartIndication = true,
         .expectedReadingCompleteIndication = true,
         .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
@@ -25,9 +40,171 @@ testCase cases[] = {
 
         .expectedGPSReading = (gpsReading){
             .error = NMEASentenceErr_processGPSNMEASentence_NoError,
-            .lat = 12.5018,
+            .lat = -10.4145,
+            .latDirection = 'S',
+            .lon = 113.6336,
+            .lonDirection = 'E',
+        },
+    },
+    {
+        .inputTestString = "$GNRMC,112738.000,A,8839.0276,N,06316.7489,E,0.00,0.00,090619,,,A*07\r\n",
+        .expectedReadingStartIndication = true,
+        .expectedReadingCompleteIndication = true,
+        .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
+
+        .expectedPostParse_NMEASentenceErr = NMEASentenceErr_NoError,
+        .expectedNMEATalkerIdentifier = "GN",
+        .expectedNMEASentenceIdentifier = "RMC",
+
+        .expectedGPSReading = (gpsReading){
+            .error = NMEASentenceErr_processGPSNMEASentence_NoError,
+            .lat = 88.6505,
             .latDirection = 'N',
-            .lon = -76.5501,
+            .lon = 63.2791,
+            .lonDirection = 'E',
+        },
+    },
+    {
+        .inputTestString = "$GNRMC,112738.000,A,1825.7489,S,11710.4031,E,0.00,0.00,090619,,,A*68\r\n",
+        .expectedReadingStartIndication = true,
+        .expectedReadingCompleteIndication = true,
+        .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
+
+        .expectedPostParse_NMEASentenceErr = NMEASentenceErr_NoError,
+        .expectedNMEATalkerIdentifier = "GN",
+        .expectedNMEASentenceIdentifier = "RMC",
+
+        .expectedGPSReading = (gpsReading){
+            .error = NMEASentenceErr_processGPSNMEASentence_NoError,
+            .lat = -18.4291,
+            .latDirection = 'S',
+            .lon = 117.1734,
+            .lonDirection = 'E',
+        },
+    },
+    {
+        .inputTestString = "$GNRMC,112738.000,A,7528.8625,S,08311.9673,W,0.00,0.00,090619,,,A*77\r\n",
+        .expectedReadingStartIndication = true,
+        .expectedReadingCompleteIndication = true,
+        .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
+
+        .expectedPostParse_NMEASentenceErr = NMEASentenceErr_NoError,
+        .expectedNMEATalkerIdentifier = "GN",
+        .expectedNMEASentenceIdentifier = "RMC",
+
+        .expectedGPSReading = (gpsReading){
+            .error = NMEASentenceErr_processGPSNMEASentence_NoError,
+            .lat = -75.481,
+            .latDirection = 'S',
+            .lon = -83.1995,
+            .lonDirection = 'W',
+        },
+    },
+    {
+        .inputTestString = "$GNRMC,112738.000,A,4429.9547,N,08150.6602,E,0.00,0.00,090619,,,A*73\r\n",
+        .expectedReadingStartIndication = true,
+        .expectedReadingCompleteIndication = true,
+        .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
+
+        .expectedPostParse_NMEASentenceErr = NMEASentenceErr_NoError,
+        .expectedNMEATalkerIdentifier = "GN",
+        .expectedNMEASentenceIdentifier = "RMC",
+
+        .expectedGPSReading = (gpsReading){
+            .error = NMEASentenceErr_processGPSNMEASentence_NoError,
+            .lat = 44.4992,
+            .latDirection = 'N',
+            .lon = 81.8443,
+            .lonDirection = 'E',
+        },
+    },
+    {
+        .inputTestString = "$GNRMC,112738.000,A,7606.5822,S,01641.7824,E,0.00,0.00,090619,,,A*65\r\n",
+        .expectedReadingStartIndication = true,
+        .expectedReadingCompleteIndication = true,
+        .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
+
+        .expectedPostParse_NMEASentenceErr = NMEASentenceErr_NoError,
+        .expectedNMEATalkerIdentifier = "GN",
+        .expectedNMEASentenceIdentifier = "RMC",
+
+        .expectedGPSReading = (gpsReading){
+            .error = NMEASentenceErr_processGPSNMEASentence_NoError,
+            .lat = -76.1097,
+            .latDirection = 'S',
+            .lon = 16.6964,
+            .lonDirection = 'E',
+        },
+    },
+    {
+        .inputTestString = "$GNRMC,112738.000,A,5920.3893,N,04216.9185,E,0.00,0.00,090619,,,A*72\r\n",
+        .expectedReadingStartIndication = true,
+        .expectedReadingCompleteIndication = true,
+        .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
+
+        .expectedPostParse_NMEASentenceErr = NMEASentenceErr_NoError,
+        .expectedNMEATalkerIdentifier = "GN",
+        .expectedNMEASentenceIdentifier = "RMC",
+
+        .expectedGPSReading = (gpsReading){
+            .error = NMEASentenceErr_processGPSNMEASentence_NoError,
+            .lat = 59.3398,
+            .latDirection = 'N',
+            .lon = 42.282,
+            .lonDirection = 'E',
+        },
+    },
+    {
+        .inputTestString = "$GNRMC,112738.000,A,8034.0672,N,16346.9775,E,0.00,0.00,090619,,,A*7f\r\n",
+        .expectedReadingStartIndication = true,
+        .expectedReadingCompleteIndication = true,
+        .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
+
+        .expectedPostParse_NMEASentenceErr = NMEASentenceErr_NoError,
+        .expectedNMEATalkerIdentifier = "GN",
+        .expectedNMEASentenceIdentifier = "RMC",
+
+        .expectedGPSReading = (gpsReading){
+            .error = NMEASentenceErr_processGPSNMEASentence_NoError,
+            .lat = 80.5678,
+            .latDirection = 'N',
+            .lon = 163.783,
+            .lonDirection = 'E',
+        },
+    },
+    {
+        .inputTestString = "$GNRMC,112738.000,A,1833.2617,S,04118.7367,W,0.00,0.00,090619,,,A*74\r\n",
+        .expectedReadingStartIndication = true,
+        .expectedReadingCompleteIndication = true,
+        .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
+
+        .expectedPostParse_NMEASentenceErr = NMEASentenceErr_NoError,
+        .expectedNMEATalkerIdentifier = "GN",
+        .expectedNMEASentenceIdentifier = "RMC",
+
+        .expectedGPSReading = (gpsReading){
+            .error = NMEASentenceErr_processGPSNMEASentence_NoError,
+            .lat = -18.5544,
+            .latDirection = 'S',
+            .lon = -41.3123,
+            .lonDirection = 'W',
+        },
+    },
+    {
+        .inputTestString = "$GNRMC,112738.000,A,3754.4189,N,02300.0702,W,0.00,0.00,090619,,,A*6e\r\n",
+        .expectedReadingStartIndication = true,
+        .expectedReadingCompleteIndication = true,
+        .expectedPostReadingNMEASentenceErr = NMEASentenceErr_NoError,
+
+        .expectedPostParse_NMEASentenceErr = NMEASentenceErr_NoError,
+        .expectedNMEATalkerIdentifier = "GN",
+        .expectedNMEASentenceIdentifier = "RMC",
+
+        .expectedGPSReading = (gpsReading){
+            .error = NMEASentenceErr_processGPSNMEASentence_NoError,
+            .lat = 37.907,
+            .latDirection = 'N',
+            .lon = -23.0012,
             .lonDirection = 'W',
         },
     },
